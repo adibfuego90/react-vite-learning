f7cccf1b8aa10d93fd1d19d078c0d783ef7a6b1e
adibfuego90 <adiburraman11@gmail.com>
2023-05-23 11:11:45 +0600

    add blog section and style

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,34 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("adibur");
-  const handleClick = () => {
-    setName("adibur rahman ");
-    console.log(name);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My name is adib",
+      body: "lorem ipsum ...Home....",
+      author: "adib",
+      id: 1,
+    },
+    {
+      title: "My name is adib",
+      body: "lorem ipsum ...Home....",
+      author: "adib",
+      id: 2,
+    },
+    {
+      title: "My name is adib",
+      body: "lorem ipsum ...Home....",
+      author: "adib",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Home Page</h2>
-      <button onClick={handleClick}>Click me </button>
+      {blogs.map((blog) => (
+        <div className="blog-priview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>The author of this blog {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
