02d34355d764f2d7c8d3a830f914db8a29eac937
adibfuego90 <adiburraman11@gmail.com>
2023-05-23 09:31:51 +0600

    use state hook is used

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,15 @@
+import { useState } from "react";
+
 const Home = () => {
-  const clickHandler = (e) => {
-    console.log("Hello, Programmer", e.target);
-  };
-  const clickAgainHandler = (name, e) => {
-    console.log(`Hello, ${name}`, e.target);
+  const [name, setName] = useState("adibur");
+  const handleClick = () => {
+    setName("adibur rahman ");
+    console.log(name);
   };
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={clickHandler}>Click Me</button>
-      <button
-        onClick={(e) => {
-          clickAgainHandler("adib", e);
-        }}>
-        Click me again
-      </button>
+      <h2>Home Page</h2>
+      <button onClick={handleClick}>Click me </button>
     </div>
   );
 };
