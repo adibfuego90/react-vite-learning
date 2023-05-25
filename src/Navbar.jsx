002059805229550b1775047e6a6b9eaa2b275056
adibfuego90 <adiburraman11@gmail.com>
2023-05-26 00:16:53 +0600

    React router added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,19 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Md. Adibur Rahman</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
           style={{
             color: "white",
             backgroundColor: "#f1356d",
             borderRadius: "8px",
           }}>
           New Blog
-        </a>
+        </Link>
       </div>
     </nav>
   );
