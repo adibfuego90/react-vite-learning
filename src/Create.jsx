3ec6bd9015f9372c9e080534b56231b7c4c8e2a0
adibfuego90 <adiburraman11@gmail.com>
2023-05-29 21:19:48 +0600

    Controler Input

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Fuego");
   return (
     <div className="create">
       <h2>Create a new Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}></textarea>
+        <label>Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Adib">Adib</option>
+          <option value="Fuego">Fuego</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <h2>{author}</h2>
+      </form>
     </div>
   );
 };
