8c2f0339a4a0c804c2b30be54069baa8ae417e56
adibfuego90 <adiburraman11@gmail.com>
2023-06-01 20:51:49 +0600

    Blog Delete button added

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,12 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 const BlogDetails = () => {
+  const history = useHistory();
   const { id } = useParams();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading ....</div>}
@@ -16,6 +24,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
